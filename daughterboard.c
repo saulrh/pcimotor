4fbf6990d556ab9439fa6d8d3ae2cd167fb5a052
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-04-14 18:00:00 -0400

    add file for daughterboard

---
+++ daughterboard.c
@@ -0,0 +1,16 @@
+/* written by saul reynolds-haertle 2012. license is generic
+ * permissive open-source until i decide on one in particular,
+ * probably aroudn the time it goes up on github. */
+
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+
+int main(void)
+{
+}
+
+
+
