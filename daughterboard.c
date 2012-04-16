00dae3d86fde883919397b4dccf794203f8ea741
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-04-16 01:36:25 -0400

    LCD IO test: Should turn on all the LCDs.

--- daughterboard.c
+++ daughterboard.c
@@ -5,11 +5,45 @@
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/sleep.h>
+/* #include <avr/sleep.h> */
 
+#include 
+
+
+#ifndef F_CPU
+#define F_CPU 32000000
+#endif
+
+
+void clock_init(void)
+{
+}
+
+void io_init(void)
+{
+    PORTA.DIRSET = 0xFF;
+    PORTB.DIRSET = 0xFF;
+    PORTC.DIRSET = 0xFF;
+    PORTD.DIRSET = 0xFF;
+    PORTE.DIRSET = 0xFF;
+}
 
 int main(void)
 {
+    /* ============================== */
+    /* initialization =============== */
+    /* ============================== */
+    io_init();
+    
+    /* ============================== */
+    /* main loop ==================== */
+    /* ============================== */
+    for(;;)
+    {
+        PORTA.OUTSET = 0x70;    /* set PA5, PA6, PA7 high */
+        PORTC.OUTSET = 0x18;    /* set PC3, PC4 high */
+        PORTE.OUTSET = 0x03;    /* set PE3 high */
+    }
 }
 
 
