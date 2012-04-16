c2b7dc28b8be5f5e572da3b9706a61352d2e00f8
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-04-16 03:20:47 -0400

    daughterboard: write code for setting up clocks

--- daughterboard.c
+++ daughterboard.c
@@ -7,8 +7,8 @@
 #include <avr/interrupt.h>
 /* #include <avr/sleep.h> */
 
-/* #include "avr_compiler.h" */
-/* #include "clksys_driver.h" */
+#include "avr_compiler.h"
+#include "clksys_driver.h"
 
 
 #ifndef F_CPU
@@ -18,6 +18,40 @@
 
 void clock_init(void)
 {
+    /* medium-priority interrupt */
+    TCC0.INTCTRLA = (TCC0.INTCTRLA & ~TC0_OVFINTLVL_gm) | TC_OVFINTLVL_MED_gc;
+    
+    /* low-level interrupt routine in PMIC */
+    PMIC.CTRL |= PMIC_MEDLVLEN_bm;
+
+    /* start the 32MHz ring oscillator ticking */
+    /* ticks at 32MHz */
+    CLKSYS_Enable(OSC_RC32MEN_bm);
+    
+    /* set the clock prescaler to divide by 1 */
+    /* ticks at 32MHz */
+    CLKSYS_Prescalers_Config(CLK_PSADIV_1_gc, CLK_PSBCDIV_1_1_gc);
+
+    /* divide main clock source by 1024 */
+    /* ticks at 31250Hz */
+    TCC0.CTRLA = (TCC0.CTRLA & ~TC0_CLKSEL_gm) | TC_CLKSEL_DIV1024_gc;
+
+    /* count to 31250k before looping; this should be half a second */
+    /* ticks at 1Hz */
+    TCC0.PER = 31250;
+
+    /* now we wait until the 32MHz oscillator is stable */
+    do {} while (CLKSYS_IsReady(OSC_RC32MRDY_bm) == 0);
+    /* and select it */
+    CLKSYS_Main_ClockSource_Select(CLK_SCLKSEL_RC32M_gc);
+}
+
+/* when the clock triggers, toggle all the LEDs */
+ISR(TCC0_OVF_vect)
+{
+    PORTA.OUTTGL = 0x70;    /* toggle PA5, PA6, PA7 */
+    PORTC.OUTTGL = 0x18;    /* toggle PC3, PC4 */
+    PORTE.OUTTGL = 0x03;    /* toggle PE3 */
 }
 
 void io_init(void)
